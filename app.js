f23b9f54edeeee8c9824e8fe30e759c1eeae06c1
Michael Lai <mike24567@gmail.com>
2016-11-18 11:05:55 -0500

    added google maps onto page

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ angular
       $scope.test = "malaka"
     }
   ])
+
+// Defines Map and sets it to sydney
+  var map;
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+  }

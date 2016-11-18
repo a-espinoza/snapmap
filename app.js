1eefdfdde982ce76266bbb86861d4da9b4b2dc3d
Michael Lai <mike24567@gmail.com>
2016-11-18 12:19:24 -0500

    initialized protoype styling

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ angular
     }
   ])
 
-// Defines Map and sets it to sydney
+// Defines Map and sets it to Washington DC
   var map;
   function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
+      center: {lat: 38.9072, lng: -77.0369},
+      zoom: 14,
+      mapTypeId: 'roadmap'
     });
   }
